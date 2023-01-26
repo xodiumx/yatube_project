c7630ee5b5df34cff0625146a1e74a9a9cde9f16
Maksim Alekseev <am.xlexeev@gmail.com>
2023-01-26 13:17:58 +0500

    fix_2

---
+++ yatube/yatube/urls.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('', include('posts.urls', namespace='posts')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('about/', include('about.urls', namespace='about')),
+    path('likes/', include('likes.urls', namespace='likes')),
+]
+
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
+handler500 = 'core.views.server_error'
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
